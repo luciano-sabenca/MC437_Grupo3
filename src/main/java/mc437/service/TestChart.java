85a43db759ee232ee3f6c8f4e63bd41d0f0e5c51
Adalberto Alencar Albuquerque <ra115913@rush.lab.ic.unicamp.br>
2014-11-14 21:20:43 -0200

    Pagina de graficos criada.

---
+++ src/main/java/mc437/service/TestChart.java
@@ -0,0 +1,15 @@
+package mc437.service;
+
+import static com.googlecode.charts4j.Color.*;
+import static com.googlecode.charts4j.UrlUtil.normalize;
+import static org.junit.Assert.assertEquals;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestChart {
+
+}
