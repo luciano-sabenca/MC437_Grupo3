d73a7717b1ea56209f53a05fd3001b8d96f9fd38
Luciano Padua Sabenca <ra136700@mamonas.lab.ic.unicamp.br>
2014-11-14 22:22:45 -0200

    Stories

---
+++ src/test/java/stories/ResultaDosTestes.java
@@ -0,0 +1,44 @@
+package stories;
+import org.jbehave.core.annotations.*;
+public class ResultaDosTestes{
+	@Given("um test result valido")
+	@Pending
+	public void givenUmTestResultValido(){
+		 //TODO 
+	}
+	@Then("uma pagina de erro eh renderizada")
+	@Pending
+	public void thenUmaPaginaDeErroEhRenderizada(){
+		 //TODO 
+	}
+	@Then("desejo obter os dados do result")
+	@Pending
+	public void thenDesejoObterOsDadosDoResult(){
+		 //TODO 
+	}
+	@When("eu acesso a pagina que exibe informacoes do test result")
+	@Pending
+	public void whenEuAcessoAPaginaQueExibeInformacoesDoTestResult(){
+		 //TODO 
+	}
+	@Then("obter o score")
+	@Pending
+	public void thenObterOScore(){
+		 //TODO 
+	}
+	@Then("obter a quantidade de mutantes mortos					 ")
+	@Pending
+	public void thenObterAQuantidadeDeMutantesMortos(){
+		 //TODO 
+	}
+	@Given("um test result nao existente")
+	@Pending
+	public void givenUmTestResultNaoExistente(){
+		 //TODO 
+	}
+	@When("desejo obter informacoes sobre esse test result")
+	@Pending
+	public void whenDesejoObterInformacoesSobreEsseTestResult(){
+		 //TODO 
+	}
+}
