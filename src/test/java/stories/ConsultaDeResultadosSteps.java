e448a84df8b2eff66ecbc85c7d929e155c2cf773
Gustavo Cesar Nunes <ra136004@paralamas.lab.ic.unicamp.br>
2014-11-28 22:35:13 -0200

    teste

---
+++ src/test/java/stories/ConsultaDeResultadosSteps.java
@@ -0,0 +1,51 @@
+package stories;
+import org.jbehave.core.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class ConsultaDeResultadosSteps{
+	
+	private WebDriver driver = new FirefoxDriver();
+	
+	private String url;
+	
+	@Then("desejo visualizar os dados acessados do result 1")
+	@Pending
+	public void thenDesejoVisualizarOsDadosAcessadosDoResult1(){
+		 //TODO 
+	}
+	@Given("um testresult valido 1")
+	@Pending
+	public void givenUmTestresultValido1(){
+		 //TODO 
+	}
+	@Then("desejo visualizar o caso de testes D:/tmp/MGEO/TESTE_00/Teste/TS_01")
+	@Pending
+	public void thenDesejoVisualizarOCasoDeTestesDtmpMGEOTESTE_00TesteTS_01(){
+		 //TODO 
+	}
+	@Then("desejo visualizar o mutante TraArcDel0.mmap")
+	@Pending
+	public void thenDesejoVisualizarOMutanteTraArcDel0mmap(){
+		 //TODO 
+	}
+	@Then("desejo visualizar o conjunto de testes D:/tmp/MGEO/TESTE_00/Teste/TS_01")
+	@Pending
+	public void thenDesejoVisualizarOConjuntoDeTestesDtmpMGEOTESTE_00TesteTS_01(){
+		 //TODO 
+	}
+	@Then("desejo visualizar o operador de mutação TraArcDel")
+	@Pending
+	public void thenDesejoVisualizarOOperadorDeMutaçãoTraArcDel(){
+		 //TODO 
+	}
+	@When("eu acesso a pagina que exibe informacoes do testresult")
+	@Pending
+	public void whenEuAcessoAPaginaQueExibeInformacoesDoTestresult(){
+		 //TODO 
+	}
+	@Then("desejo visualizar Vivo? Sim")
+	@Pending
+	public void thenDesejoVisualizarVivoSim(){
+		 //TODO 
+	}
+}
