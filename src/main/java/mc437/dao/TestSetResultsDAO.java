f853f7f52d34db0f9c5eb9809775ddd69218900c
Luciano Padua Sabenca <ra136700@sepultura.lab.ic.unicamp.br>
2014-12-12 20:22:10 -0200

    Melhorias

--- src/main/java/mc437/dao/TestSetResultsDAO.java
+++ src/main/java/mc437/dao/TestSetResultsDAO.java
@@ -1,33 +1,49 @@
 package mc437.dao;
 
-import java.sql.Types;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.sql.DataSource;
+
 import mc437.bean.TestSetResults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TestSetResultsDAO {
-	
+
 	@Autowired
 	DataSource datasource;
-	
-	public int save(TestSetResults testSetResults){
-		String sql = "SELECT MAX(id_seq) FROM test_set_results";
-		
+
+	public int save(final TestSetResults testSetResults) {
 		JdbcTemplate template = new JdbcTemplate(datasource);
-		template.update(
-				"INSERT INTO test_set_results(cod"
-				                           + ",path"
-				                           + ",id"
-				                           + ",id_i_test_result) VALUES (?, ?, ?, ?)",
-				new Object[] { testSetResults.getCod()
-				              ,testSetResults.getPath()
-					 	      ,testSetResults.getId()
-					 	      ,testSetResults.getIdItestResult()}
-				              ,new int[] { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER });
-		
-		return template.queryForObject(sql, new Object[] {}, Integer.class);
+		KeyHolder holder = new GeneratedKeyHolder();
+		template.update(new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(
+					Connection connection) throws SQLException {
+				PreparedStatement ps = connection.prepareStatement(
+						"INSERT INTO test_set_results(cod" + ",path" + ",id"
+								+ ",id_i_test_result) VALUES (?, ?, ?, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+
+				ps.setString(1, testSetResults.getCod());
+				ps.setString(2, testSetResults.getPath());
+				ps.setString(3, testSetResults.getId());
+				ps.setInt(4, testSetResults.getIdItestResult());
+
+				return ps;
+			}
+		}, holder);
+
+		return holder.getKey().intValue();
 	}
 }
