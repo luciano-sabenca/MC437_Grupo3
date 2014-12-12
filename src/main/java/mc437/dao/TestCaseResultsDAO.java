392da7b1cb2cc11c11c9319f63f1dac5633e5c91
Luan Gustavo Bicesto <ra136576@mamonas.lab.ic.unicamp.br>
2014-12-12 18:50:05 -0200

    t

--- src/main/java/mc437/dao/TestCaseResultsDAO.java
+++ src/main/java/mc437/dao/TestCaseResultsDAO.java
@@ -1,6 +1,9 @@
 package mc437.dao;
 
-import java.sql.Types;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
@@ -8,6 +11,9 @@ import mc437.bean.TestCaseResults;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,21 +22,31 @@ public class TestCaseResultsDAO {
 	@Autowired
 	DataSource datasource;
 	
-	public int save(TestCaseResults testCaseResults){
-		String sql = "SELECT MAX(id_seq) FROM test_case_results";
-		
+	public int save(final TestCaseResults testCaseResults){
+		KeyHolder holder = new GeneratedKeyHolder();
 		JdbcTemplate template = new JdbcTemplate(datasource);
 		template.update(
-				"INSERT INTO test_case_results(test_case_key"
+				new PreparedStatementCreator() {
+
+					@Override
+					public PreparedStatement createPreparedStatement(
+							Connection connection) throws SQLException {
+						PreparedStatement ps = connection.prepareStatement(
+								"INSERT INTO test_case_results(test_case_key"
 				                           + ",path"
 				                           + ",id_test_set_results"
 				                           + ",id_i_test_results) VALUES (?, ?, ?, ?)",
-				new Object[] { testCaseResults.getTestCaseKey()
-					 	      ,testCaseResults.getPath()
-					 	      ,testCaseResults.getIdTestSetResults()
-					 	      ,testCaseResults.getIdITestResults()}
-				              ,new int[] {Types.VARCHAR, Types.VARCHAR, Types.INTEGER, Types.INTEGER });
-		
-		return template.queryForObject(sql, new Object[] {}, Integer.class);
+								Statement.RETURN_GENERATED_KEYS);
+
+						ps.setString(1, testCaseResults.getTestCaseKey());
+						ps.setString(2, testCaseResults.getPath());
+						ps.setInt(3, testCaseResults.getIdTestSetResults());
+						ps.setInt(4, testCaseResults.getIdITestResults());
+
+						return ps;
+					}
+				}, holder);
+
+				return holder.getKey().intValue();
 	}
 }
